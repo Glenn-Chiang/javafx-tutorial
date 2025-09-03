61a8cdb39e82a85b3fb971ee87076f154d507962
Glenn <glennchiang24@gmail.com>
2025-09-03 21:10:29 +0800

    Add DialogBox

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -15,6 +16,9 @@ public class Main extends Application {
     private Button sendButton;
     private Scene scene;
 
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
     @Override
     public void start(Stage stage) {
         scrollPane = new ScrollPane();
@@ -24,6 +28,9 @@ public class Main extends Application {
         userInput = new TextField();
         sendButton = new Button("Send");
 
+        DialogBox dialogBox = new DialogBox("Hello!", userImage);
+        dialogContainer.getChildren().addAll(dialogBox);
+
         AnchorPane mainLayout = new AnchorPane();
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
 
@@ -31,6 +38,6 @@ public class Main extends Application {
 
         stage.setScene(scene);
         stage.show();
-        
+
     }
 }
